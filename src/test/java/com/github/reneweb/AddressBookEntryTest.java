e44fc84620c9ff517507029973c3680686a056f2
Rene <weber.rene@live.de>
2014-08-27 01:49:54 +0200

    Created test classes

---
+++ src/test/java/com/github/reneweb/AddressBookEntryTest.java
@@ -0,0 +1,13 @@
+package com.github.reneweb;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AddressBookEntryTest {
+
+    @Test
+    public void testAgeDifferenceFrom() throws Exception {
+
+    }
+}
---
+++ src/test/java/com/github/reneweb/AddressBookTest.java
@@ -0,0 +1,43 @@
+package com.github.reneweb;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AddressBookTest {
+
+    @Test
+    public void testReadAddressBookFromFile() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEntriesByFullName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEntriesByFirstName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetEntriesByLastName() throws Exception {
+
+    }
+
+    @Test
+    public void testFilterByGender() throws Exception {
+
+    }
+
+    @Test
+    public void testGetOldestPerson() throws Exception {
+
+    }
+
+    @Test
+    public void testSearchAddressBook() throws Exception {
+
+    }
+}
