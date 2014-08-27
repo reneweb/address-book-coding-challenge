be8cda8b173cddac8cf9c23310af86d6c1d0a69b
Rene <weber.rene@live.de>
2014-08-27 02:13:42 +0200

    Implemented tests for AddressBookEntry

--- src/test/java/com/github/reneweb/AddressBookEntryTest.java
+++ src/test/java/com/github/reneweb/AddressBookEntryTest.java
@@ -2,12 +2,18 @@ package com.github.reneweb;
 
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 import static org.junit.Assert.*;
 
 public class AddressBookEntryTest {
 
     @Test
     public void testAgeDifferenceFrom() throws Exception {
-
+        AddressBookEntry firstEntry = new AddressBookEntry("Test", "Test", AddressBookEntry.Gender.MALE, LocalDate.of(1980, 1, 1));
+        AddressBookEntry secondEntry = new AddressBookEntry("Test", "Test", AddressBookEntry.Gender.MALE, LocalDate.of(1970, 1, 1));
+        Period ageDifference = firstEntry.ageDifferenceFrom(secondEntry);
+        assertTrue(ageDifference.getDays() == 0 && ageDifference.getMonths() == 0 && ageDifference.getYears() == 10);
     }
 }
